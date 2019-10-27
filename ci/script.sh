6d79893b7c7f5e3d3d4bb9e7bae0f68d0ae5346e
zoidbergwill <zoidbergwill@gmail.com>
2019-10-27 17:44:24 +0100

    Use Rust target for 32 or 64 bit (#1)

--- ci/script.sh
+++ ci/script.sh
@@ -2,14 +2,21 @@
 
 set -ex
 
-if [[ "$TRAVIS_RUST_ARCHITECTURE" == "i386" ]]; then
-  apt-get update
-  apt-get install -y gcc-multilib
-  rustup target add i686-unknown-linux-gnu
+RUST_TARGET="x86_64-unknown-linux-gnu"
+
+if [ "${TRAVIS_RUST_ARCHITECTURE}" = "i386" ]; then
+  RUST_TARGET="i686-unknown-linux-gnu"
 fi
 
-cargo build --verbose
-cargo doc --verbose
+if [ "${RUST_TARGET}" = "i686-unknown-linux-gnu" ]; then
+  apt-get update
+  apt-get install -y gcc-multilib
+fi
+
+rustup target add ${RUST_TARGET}
+
+cargo build --verbose --target "${RUST_TARGET}"
+cargo doc --verbose --target "${RUST_TARGET}"
 
 # If we're testing on an older version of Rust, then only check that we
 # can build the crate. This is because the dev dependencies might be updated
@@ -20,7 +27,7 @@ if [ "$TRAVIS_RUST_VERSION" = "1.21.0" ]; then
   exit
 fi
 
-cargo test --verbose
+cargo test --verbose --target "${RUST_TARGET}"
 if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
-  cargo bench --verbose --no-run
+  cargo bench --verbose --no-run --target "${RUST_TARGET}"
 fi
